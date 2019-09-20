f74326d0e459e2015d07095632bb115575d27c5f
Karthikeyan <karthikeyan.live@hotmail.com>
2019-09-20 19:18:41 +0530

    sample snapshot testing - jest

---
+++ src/components/FriendListPaginate.Snapshots.test.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import renderer from 'react-test-renderer';
+import FriendListPaginate from './FriendListPaginate';
+
+function renderFriendListPaginate(args) {
+    let defaultProps = {
+        activePage: 1,
+        itemsCountPerPage: 2,
+        totalItemsCount: 3,
+        changeActivePage: () => { }
+    };
+    const props = { ...defaultProps, ...args };
+    return renderer.create(<FriendListPaginate {...props} />)
+}
+
+it('with default props component is rendered', () => {
+    const component = renderFriendListPaginate();
+    const json = component.toJSON();
+    expect(json).toMatchSnapshot();
+});
+
+it('navigate to 2nd page and check if legend updated', () => {
+    const component = renderFriendListPaginate({activePage:2});
+    const json = component.toJSON();
+    expect(json).toMatchSnapshot();
+});
