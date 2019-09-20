7c1a7ae4cbf797b247615266d728e9e2765f34bd
Karthikeyan <karthikeyan.live@hotmail.com>
2019-09-20 17:14:57 +0530

    Few unit testing added (react-testing-library)

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+it('renders welcome message', () => {
+  const { getByText } = render(<App />);
+  expect(getByText('Welcome to React')).toBeInTheDocument();
 });
